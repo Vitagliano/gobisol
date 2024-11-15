8fba48ee7bf5e5ba75689c7c8a7c39cb0438d4bc
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2024-11-15 12:13:15 -0300

    fixed website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,23 @@
 
 export default function Home() {
   return (
-    <div className="relative overflow-hidden m-0">
-      <div className="relative h-[40vh] md:h-dvh w-full flex justify-center items-start">
+    <div className="relative overflow-hidden m-0 sm:min-h-dvh flex flex-col justify-between h-dvh md:h-full">
+      <div className="relative md:h-[65dvh] h-[40vh] w-full flex justify-center items-start">
         <img
           id="logo"
           src="/images/logo.png"
           alt="Gobi"
           draggable={false}
-          className="w-1/3 pt-8 md:pt-24"
+          className="w-1/2 mt-16 md:mt-8 relative z-10"
         />
         <img
           src="/images/clouds.png"
           alt="Clouds"
           draggable={false}
-          className="absolute z-0 min-w-screen w-full h-auto"
+          className="absolute z-0 min-w-screen w-full h-auto top-[35%] md:top-[15%]"
         />
       </div>
-      <div className="relative h-full w-full" id="content">
+      <div className="relative md:h-full w-ful" id="content">
         <img
           src="/images/forest-front.png"
           alt="Gobi on the forest"
@@ -37,7 +37,7 @@ export default function Home() {
           draggable={false}
           className="absolute z-3 h-auto w-[30.5%] bottom-0 right-[26.6%]"
         />
-        <a href="https://x.com/gobi" target="_blank" className="group">
+        <a href="https://x.com/" target="_blank" className="group">
           <img
             src="/images/x.png"
             alt="X (Twitter)"
@@ -51,11 +51,7 @@ export default function Home() {
             className="absolute z-3 h-auto w-[19%] bottom-[10.5%] left-[6.2%] opacity-0 group-hover:opacity-100 "
           />
         </a>
-        <a
-          href="https://dexscreener.com/solana/gobi"
-          target="_blank"
-          className="group"
-        >
+        <a href="https://dexscreener.com/" target="_blank" className="group">
           <img
             src="/images/dex.png"
             alt="Dexscreener"
@@ -69,7 +65,7 @@ export default function Home() {
             className="absolute z-3 h-auto w-[15.3%] bottom-[20.75%] left-[27.5%] opacity-0 group-hover:opacity-100 "
           />
         </a>
-        <a href="https://t.me/gobi" target="_blank" className="group">
+        <a href="https://t.me/" target="_blank" className="group">
           <img
             src="/images/tg.png"
             alt="Telegram"
