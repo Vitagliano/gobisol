ecf509cc956f16cb9f0ae0a5135f084cf06bab08
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2024-11-15 11:24:08 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,94 @@
+"use client";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="relative overflow-hidden m-0">
+      <div className="relative h-[40vh] md:h-dvh w-full flex justify-center items-start">
+        <img
+          id="logo"
+          src="/images/logo.png"
+          alt="Gobi"
+          draggable={false}
+          className="w-1/3 pt-8 md:pt-24"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <img
+          src="/images/clouds.png"
+          alt="Clouds"
+          draggable={false}
+          className="absolute z-0 min-w-screen w-full h-auto"
+        />
+      </div>
+      <div
+        className="relative h-full w-full"
+        id="content"
+      >
+        <img
+          src="/images/forest-front.png"
+          alt="Gobi on the forest"
+          draggable={false}
+          className="relative w-full h-auto z-1 "
+        />
+        <img
+          src="/images/forest-back.png"
+          alt="Gobi on the forest"
+          draggable={false}
+          className="absolute w-full h-auto z-0 bottom-0"
+        />
+        <img
+          src="/images/gobi.png"
+          alt="Gobi"
+          draggable={false}
+          className="absolute z-3 h-auto w-[30.5%] bottom-0 right-[26.6%]"
+        />
+        <a href="https://x.com/gobi" target="_blank" className="group">
+          <img
+            src="/images/x.png"
+            alt="X (Twitter)"
+            draggable={false}
+            className="absolute z-2 h-auto w-[19%] bottom-[10.5%] left-[6.2%] opacity-100 group-hover:opacity-0 "
           />
-          Learn
+          <img
+            src="/images/x-hover.png"
+            alt="X (Twitter)"
+            draggable={false}
+            className="absolute z-3 h-auto w-[19%] bottom-[10.5%] left-[6.2%] opacity-0 group-hover:opacity-100 "
+          />
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://dexscreener.com/solana/gobi"
           target="_blank"
-          rel="noopener noreferrer"
+          className="group"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <img
+            src="/images/dex.png"
+            alt="Dexscreener"
+            draggable={false}
+            className="absolute z-2 h-auto w-[15.3%] bottom-[20.75%] left-[27.5%] opacity-100 group-hover:opacity-0 "
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+          <img
+            src="/images/dex-hover.png"
+            alt="Dexscreener"
+            draggable={false}
+            className="absolute z-3 h-auto w-[15.3%] bottom-[20.75%] left-[27.5%] opacity-0 group-hover:opacity-100 "
           />
-          Go to nextjs.org →
         </a>
-      </footer>
+        <a href="https://t.me/gobi" target="_blank" className="group">
+          <img
+            src="/images/tg.png"
+            alt="Telegram"
+            draggable={false}
+            className="absolute z-2 h-auto w-[20.85%] bottom-[24.8%] right-[8.08%] opacity-100 group-hover:opacity-0 "
+          />
+          <img
+            src="/images/tg-hover.png"
+            alt="Telegram"
+            draggable={false}
+            className="absolute z-3 h-auto w-[20.85%] bottom-[24.8%] right-[8.08%] opacity-0 group-hover:opacity-100 "
+          />
+        </a>
+      </div>
     </div>
   );
 }
